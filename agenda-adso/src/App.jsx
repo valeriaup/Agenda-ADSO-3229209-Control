77ca31b48737916950cc29b8f687b03e75011276
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-02-15 21:15:47 -0500

    Primer Commit

---
+++ agenda-adso/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App(){ const fecha = new Date().toLocaleString(); return (
+  <main>
+    <h1>Hola soy Valeria aprendiz de ADSO en el SENA</h1>
+    <p>
+      Espero aprender las bases de React para hacer más interactivos mis proyectos.
+    </p>
+  </main>
+)}
